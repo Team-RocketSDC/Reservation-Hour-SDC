4b14f3e9d92a7d709a955279ea4012ed665f3fb7
keshavmandayam <keshavmandayam@gmail.com>
2018-11-30 10:08:22 -0800

    slight modifications to data structure

--- reservation-hour/client/index.jsx
+++ reservation-hour/client/index.jsx
@@ -44,77 +44,76 @@ class App extends Component {
     super();
     this.state = {
       reservations: [],
-      hours: [],
+      hours: []
     };
   }
 
   componentDidMount() {
     const restaurantId = window.location.pathname.slice(1);
-    axios
-      .get(`http://localhost:5882/api/${restaurantId}/reservation`)
-      .then((response) => {
-        const reservations = response.data[0].reservations.map(reservation => Object.assign({}, reservation, {
-            time: moment(reservation.time).format('kk:mm'),
-            name: response.data[0].name,
-            date: moment(reservation.time).format('MM YYYY'),
-          }),);
-        axios
-          .get(`http://localhost:5882/api/${restaurantId}/hour`)
-          .then((result) => {
-            const data = [
-              {
-                weekday: 0,
-                openingHour: result.data[0].monday.slice(0, 8),
-                closingHour: result.data[0].monday.slice(11, 19),
-                name: result.data[0].name,
-              },
-              {
-                weekday: 1,
-                openingHour: result.data[0].tuesday.slice(0, 8),
-                closingHour: result.data[0].tuesday.slice(11, 19),
-                name: result.data[0].name,
-              },
-              {
-                weekday: 2,
-                openingHour: result.data[0].wednesday.slice(0, 8),
-                closingHour: result.data[0].wednesday.slice(11, 19),
-                name: result.data[0].name,
-              },
-              {
-                weekday: 3,
-                openingHour: result.data[0].thursday.slice(0, 8),
-                closingHour: result.data[0].thursday.slice(11, 19),
-                name: result.data[0].name,
-              },
-              {
-                weekday: 4,
-                openingHour: result.data[0].friday.slice(0, 8),
-                closingHour: result.data[0].friday.slice(11, 19),
-                name: result.data[0].name,
-              },
-              {
-                weekday: 5,
-                openingHour: result.data[0].saturday.slice(0, 8),
-                closingHour: result.data[0].saturday.slice(11, 19),
-                name: result.data[0].name,
-              },
-              {
-                weekday: 6,
-                openingHour: result.data[0].sunday.slice(0, 8),
-                closingHour: result.data[0].sunday.slice(11, 19),
-                name: result.data[0].name,
-              },
-            ];
-            const hours = data.map(weekday => Object.assign({}, weekday, {
-                opening_hour: weekday.openingHour.slice(0, 5),
-                closing_hour: weekday.closingHour.slice(0, 5),
-              }),);
-            this.setState({
-              reservations,
-              hours,
-            });
-          });
+    axios.get(`/api/${restaurantId}/reservation`).then(response => {
+      const reservations = response.data.map(reservation =>
+        Object.assign({}, reservation, {
+          time: moment(reservation.time).format('kk:mm'),
+          date: moment(reservation.time).format('MM YYYY')
+        })
+      );
+      axios.get(`/api/${restaurantId}/hour`).then(result => {
+        const data = [
+          {
+            weekday: 0,
+            openingHour: result.data[0].monday.slice(0, 8),
+            closingHour: result.data[0].monday.slice(11, 19),
+            name: result.data[0].name
+          },
+          {
+            weekday: 1,
+            openingHour: result.data[0].tuesday.slice(0, 8),
+            closingHour: result.data[0].tuesday.slice(11, 19),
+            name: result.data[0].name
+          },
+          {
+            weekday: 2,
+            openingHour: result.data[0].wednesday.slice(0, 8),
+            closingHour: result.data[0].wednesday.slice(11, 19),
+            name: result.data[0].name
+          },
+          {
+            weekday: 3,
+            openingHour: result.data[0].thursday.slice(0, 8),
+            closingHour: result.data[0].thursday.slice(11, 19),
+            name: result.data[0].name
+          },
+          {
+            weekday: 4,
+            openingHour: result.data[0].friday.slice(0, 8),
+            closingHour: result.data[0].friday.slice(11, 19),
+            name: result.data[0].name
+          },
+          {
+            weekday: 5,
+            openingHour: result.data[0].saturday.slice(0, 8),
+            closingHour: result.data[0].saturday.slice(11, 19),
+            name: result.data[0].name
+          },
+          {
+            weekday: 6,
+            openingHour: result.data[0].sunday.slice(0, 8),
+            closingHour: result.data[0].sunday.slice(11, 19),
+            name: result.data[0].name
+          }
+        ];
+        const hours = data.map(weekday =>
+          Object.assign({}, weekday, {
+            opening_hour: weekday.openingHour.slice(0, 5),
+            closing_hour: weekday.closingHour.slice(0, 5)
+          })
+        );
+        this.setState({
+          reservations,
+          hours
+        });
       });
+    });
   }
 
   render() {
--- reservation-hour/data/seedReservations2.js
+++ reservation-hour/data/seedReservations2.js
@@ -3,33 +3,109 @@ const faker = require('faker');
 const moment = require('moment');
 const uuidv1 = require('uuid/v1');
 
-const genTimes = () =>
-  `${faker.random.number({
+const genTimes = () => `0${faker.random.number({
     min: 5,
-    max: 10
+    max: 9,
   })}:00:00 - ${faker.random.number({ min: 15, max: 24 })}:00:00`;
 
 const genRes = () => {
   const reservations = [];
+  // const reservations = {};
   for (let i = 0; i < 10; i += 1) {
+    // const name = faker.name.findName().replace(/'/g, '');
+    // const time = moment(faker.date.recent(90)).format('YYYY-MM-DD hh:mm:ss');
+    // reservations[name] = time;
     const reservation = {
       reservee: faker.name.findName().replace(/'/g, ''),
-      time: moment(faker.date.recent(90)).format('YYYY-MM-DD hh:mm:ss')
+      time: moment(faker.date.recent(90)).format('YYYY-MM-DD hh:mm:ss'),
     };
-    reservations.push(`'${JSON.stringify(reservation).replace(/"/g, '')}'`);
+    // reservations.push(`'${JSON.stringify(reservation).replace(/"/g, '')}'`);
+    reservations.push(reservation);
   }
   return reservations;
 };
 
 const hoursSeed = () => {
-  const stream = fs.createWriteStream('hourstest2.csv');
-  let i = 0;
-  stream.write('id|name|reservations\n');
+  // const stream = fs.createWriteStream('hourstest2.csv');
+  faker.seed(123);
+  const stream1 = fs.createWriteStream('hours3.csv');
+  let i = 2000000;
+  // stream.write('id|name|reservations\n');
+  stream1.write(
+    'id|name|Monday|Tuesday|Wednesday|Thursday|Friday|Saturday|Sunday\n',
+  );
   function write() {
-    while (i < 10) {
+    while (i < 3000000) {
       // 10 million hours
       i += 1;
-      const string = `${uuidv1()}|${faker.commerce.productName()}|[${genRes()}]\n`;
+      // const id = uuidv1();
+      const name = faker.commerce.productName();
+      const reservations = genRes();
+      const monday = genTimes();
+      const tuesday = genTimes();
+      const wednesday = genTimes();
+      const thursday = genTimes();
+      const friday = genTimes();
+      const saturday = genTimes();
+      const sunday = genTimes();
+      // const string = `${id}|${name}|[${genRes()}]\n`;
+      const string1 = `${i}|${name}|${monday}|${tuesday}|${wednesday}|${thursday}|${friday}|${saturday}|${sunday}\n`;
+      if (!stream1.write(string1)) {
+        return;
+      }
+    }
+    stream1.end();
+  }
+  stream1.on('drain', () => {
+    write();
+  });
+  write();
+};
+
+const resSeed = () => {
+  faker.seed(123);
+  const stream = fs.createWriteStream('reservations4.csv');
+  // const stream1 = fs.createWriteStream('hourstest3.csv');
+  let i = 0;
+  stream.write('id|name|reservee|time\n');
+  // stream1.write(
+  //   'id|name|Monday|Tuesday|Wednesday|Thursday|Friday|Saturday|Sunday\n'
+  // );
+
+  function write() {
+    while (i < 10000000) {
+      // 10 million hours
+      i += 1;
+      // const name = faker.commerce.productName();
+      const name = faker.commerce.productName();
+      const reservations = genRes();
+      const monday = genTimes();
+      const tuesday = genTimes();
+      const wednesday = genTimes();
+      const thursday = genTimes();
+      const friday = genTimes();
+      const saturday = genTimes();
+      const sunday = genTimes();
+      const string = `${i}|${name}|${reservations[0].reservee}|${
+        reservations[0].time
+      }\n${i}|${name}|${reservations[1].reservee}|${
+        reservations[1].time
+      }\n${i}|${name}|${reservations[2].reservee}|${
+        reservations[2].time
+      }\n${i}|${name}|${reservations[3].reservee}|${
+        reservations[3].time
+      }\n${i}|${name}|${reservations[4].reservee}|${
+        reservations[4].time
+      }\n${i}|${name}|${reservations[5].reservee}|${
+        reservations[5].time
+      }\n${i}|${name}|${reservations[6].reservee}|${
+        reservations[6].time
+      }\n${i}|${name}|${reservations[7].reservee}|${
+        reservations[7].time
+      }\n${i}|${name}|${reservations[8].reservee}|${
+        reservations[8].time
+      }\n${i}|${name}|${reservations[9].reservee}|${reservations[9].time}\n`;
+      // const string1 = `${id}|${name}|${genTimes()}|${genTimes()}|${genTimes()}|${genTimes()}|${genTimes()}|${genTimes()}|${genTimes()}\n`;
       if (!stream.write(string)) {
         return;
       }
@@ -42,6 +118,6 @@ const hoursSeed = () => {
   write();
 };
 
-hoursSeed();
+resSeed();
 
-exports.hoursSeed = hoursSeed;
+exports.resSeed = resSeed;
